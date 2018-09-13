0393ef4e9746f10f12f6ce387659a11b74a1d227
Wenqing Zhang <wenqing.zhang@thoughtworks.com>
2018-09-13 17:52:46 +0800

    mvc demo

---
+++ 03 - mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _03___mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
