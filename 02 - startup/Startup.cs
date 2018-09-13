f0455a6255aabc308955a1d13ec195ced00e765a
Wenqing Zhang <wenqing.zhang@thoughtworks.com>
2018-09-13 17:51:52 +0800

    add startup demo

---
+++ 02 - startup/Startup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace _02___startup
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+        private readonly ILoggerFactory _loggerFactory;
+
+        // this injection be handled by IWebHostBuilder
+        public Startup(IConfiguration config, ILoggerFactory loggerFactory)
+        {
+            _config = config;
+            _loggerFactory = loggerFactory;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                // Set a short timeout for easy testing.
+                options.IdleTimeout = TimeSpan.FromSeconds(30);
+                options.Cookie.HttpOnly = true;
+            });
+            //services.AddScoped<CustomerTracker>()
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSession();
+
+            app.Map("/endpoint", (builder) =>
+            {
+                builder.Run(async (context) => {
+                    await context.Response.WriteAsync("Hello in Map");
+                });
+            });
+
+            // This is the last fallback middleware
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello in last Run!");
+            });
+        }
+    }
+}
